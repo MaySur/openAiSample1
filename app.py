0491427ec10fbade9b6bf99b9953c7fee0854fad
Mayur Suresh <mayursuresh2022@gmail.com>
2023-04-04 11:30:41 -0400

    first

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+
+import openai
+from flask import Flask, redirect, render_template, request, url_for
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.route("/", methods=("GET", "POST"))
+def index():
+    if request.method == "POST":
+        question = request.form["question"]
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=generate_prompt(question),
+            temperature=1,
+        )
+        return redirect(url_for("index", result=response.choices[0].text))
+
+    result = request.args.get("result")
+    return render_template("index.html", result=result)
+
+
+def generate_prompt(question):
+    return """Suggest some simple answers.
+
+Question: What is the Earth?
+Answer: That is the planet we live on.
+Question: What is the sun?
+Answer: That is the closet start to us.
+Question: What is the weather?
+Answer: It feels like 78°
+Question: {}
+Answer:""".format(
+        question.capitalize()
+    )
